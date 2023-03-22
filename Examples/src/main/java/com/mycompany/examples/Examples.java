da801319aaa7f4869a33b6b004de02ff8fddbcef
Prasid-122 <97814916+Prasid-122@users.noreply.github.com>
2023-03-22 13:02:19 +0545

    remove folder

--- Examples/src/main/java/com/mycompany/examples/Examples.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.examples;
-
-public class Examples {
-
-    public static void main(String[] args) {
-    new trys();
-    }
-}
--- Examples/src/main/java/com/mycompany/examples/trys.java
+++
@@ -1,7 +0,0 @@
-
-package com.mycompany.examples;
-
-
-public class trys {
-    
-}
